cd9d363feaa6709382f0911ebd4b0480efcefbdc
narsi <narsihuang@gmail.com>
2026-08-20 19:41:30 +0800

    76ers

--- internal/consts/crawler.go
+++ internal/consts/crawler.go
@@ -19,7 +19,7 @@ var TeamIDToName = map[int]string{
 
 // AllCrawlTeamIDs 需要抓取的所有球队ID（固定顺序）
 var AllCrawlTeamIDs = []int{
-	// 0,
+	0,
 	1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 	21, 22, 23, 24, 25, 26, 27, 28, 29, 31, TeamIDFreeAgent,
 }
